100520ac54ae3deb59e6bca93571e41a87afb8bd
nehapalwe <chanchalpalwe468@gmail.com>
2024-10-23 21:50:15 +0530

    DOM project

---
+++ DOM_1/script.js
@@ -0,0 +1,18 @@
+
+let divElement = document.getElementById("target-div");
+
+divElement.style.backgroundColor = "yellow";
+
+divElement.style.margin = "20px";
+
+divElement.style.padding = "10px";
+
+divElement.style.fontSize = "18px";
+
+divElement.style.fontColour = "blue";
+
+divElement.style.fontWeight = "bold";
+
+divElement.style.height = "200px";
+
+divElement.style.width = "300px";
